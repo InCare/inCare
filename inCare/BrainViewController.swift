1a47bc0196c71b4e7d6b03bf17b65a519427167d
Andrew Carvajal <andrewcarvajal1@gmail.com>
2019-08-15 16:04:28 -0400

    Initial commit

---
+++ inCare/BrainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BrainViewController.swift
+//  inCare
+//
+//  Created by Andrew Carvajal on 8/15/19.
+//  Copyright © 2019 inCare. All rights reserved.
+//
+
+import UIKit
+
+class BrainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
